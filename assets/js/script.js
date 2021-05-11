fed1e634213506922e824e06cb226263760c5b24
Jsieler <joshuasieler@Joshs-Air.home>
2021-05-11 17:20:49 -0600

    alert if input box is empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,20 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
-  event.preventDefault();
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
-  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+  return false;
+}
 
+formEl.reset();
+
+ 
+  
   // package up data as an object
   var taskDataObj = {
       name: taskNameInput,
@@ -15,6 +25,7 @@ var taskFormHandler = function(event) {
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 };
+
 
 var createTaskEl = function (taskDataObj) {
   // create list item
